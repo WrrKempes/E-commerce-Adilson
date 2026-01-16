a74a0bad14ecf477570eca5ab1476987b75f3298
Wrrkempes <nicolaseduardo1914@gmail.com>
2026-01-15 23:26:28 -0300

    first commit

---
+++ backend/src/main/kotlin/com/nicolaseduardo/e_commerce_adilson/ECommerceAdilsonApplication.kt
@@ -0,0 +1,11 @@
+package com.nicolaseduardo.e_commerce_adilson
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class ECommerceAdilsonApplication
+
+fun main(args: Array<String>) {
+	runApplication<ECommerceAdilsonApplication>(*args)
+}
---
+++ backend/src/test/kotlin/com/nicolaseduardo/e_commerce_adilson/ECommerceAdilsonApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.nicolaseduardo.e_commerce_adilson
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class ECommerceAdilsonApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
